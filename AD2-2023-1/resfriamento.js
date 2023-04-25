e18784597cccfba0fe55c23ef8b4c99bad5637ac
mateusguida <guidamaster@gmail.com>
2023-04-25 18:36:01 -0300

    Questão 1 - AD2

---
+++ AD2-2023-1/resfriamento.js
@@ -0,0 +1,126 @@
+/*var tabLentes = [
+  ['Softlens','Bauch & Lonb', 30, 100, 'BauchELomb_30D_200'],
+  ['Bioninfinity','Coopervision', 30, 100, 'Bioinfinity_30D_200'],
+  ['Acuvue Oasys','Johnson & Johnson', 15, 220, 'Acuvue_15D_200'],
+  ['Acuvue 2','Johnson & Johnson', 15, 170, 'Acuvue2_15D_200']
+];
+
+var tabArmacoes = [
+  ['Oakley Wingfold', 'M_Oakley_100', 'Masculino', 855],
+  ['Persol', 'M_Persol_100', 'Masculino', 1377],
+  ['Ray-ban Round', 'M_Ray-ban_100', 'Masculino', 666],
+  ['Kipling', 'F_Kipling_100', 'Feminino', 423],
+  ['Michael Kors', 'F_MichaelKors_100', 'Feminino', 684]
+];*/
+
+//var valor = 0;
+
+/*
+* QUESTÃO 1
+* Função chamada para abrir os pop-ups
+* da página Tipos
+*/
+function abreJanTipo(){
+  var jan = window.open("","","width=250, height=200");
+  
+  jan.document.write("<html> <head> <meta charset='utf-8'>");
+  jan.document.write("<link rel='stylesheet' type='text/css' href='Estilos/refrig.css'>");
+  jan.document.write("<link rel='stylesheet' type='text/css' href='Estilos/tipos.css'> </head>");
+  jan.document.write(`<body><div class="janInfo"><h3>Tipos de<br/>Ar-condicionado`);
+  jan.document.write(`</h3></div><ul><li>De parede</li><li>Splitter</li>`);
+  jan.document.write(`<li>Portátil</li></ul><div class="janInfo">`);
+  jan.document.write("<button onclick='window.close();'>Fechar</button></div></body></html>");
+}
+
+function abreJanCapac(){
+  var jan = window.open("","","width=340, height=320");
+  
+  jan.document.write("<html> <head> <meta charset='utf-8'>");
+  jan.document.write("<link rel='stylesheet' type='text/css' href='Estilos/refrig.css'>");
+  jan.document.write("<link rel='stylesheet' type='text/css' href='Estilos/tipos.css'>");
+  jan.document.write(`</head><body><div class="janInfo"><h3>Tamanho do Cômodo</h3>`);
+  jan.document.write(`<table class="tabInfo"><thead><tr>`);
+  jan.document.write(`<th rowspan="2" class="linTH">Área</th>`);
+  jan.document.write(`<th colspan="2" class="linTH">Incidência do Sol</th></tr>`);
+  jan.document.write(`<tr><th class="linTH">Manhã</th>`);
+  jan.document.write(`<th class="linTH">Tarde</th></tr>`);
+  jan.document.write(`</thead><tbody><tr><td>9 m2</td>`);
+  jan.document.write(`<td>7500 BTUs</td><td>9000 BTUs</td>`);
+  jan.document.write(`</tr><tr><td>12 m2</td><td>9000 BTUs</td>`);
+  jan.document.write(`<td>10000 BTUs</td></tr><tr>`);
+  jan.document.write(`<td>25 m2</td><td>12000 BTUs</td>`);
+  jan.document.write(`<td>15000 BTUs</td></tr><tr>`);
+  jan.document.write(`<td>50 m2</td><td>21000 BTUs</td>`);
+  jan.document.write(`<td>30000 BTUs</td></tr></tbody></table>`);
+  jan.document.write("<br/><button onclick='window.close();'>Fechar");
+  jan.document.write("</button></div></body></html>");
+}
+
+/*
+* QUESTÃO 2
+* Função chamada para exibir as
+* imagens e texto na tabela
+*/
+/*
+function mostra(cod){
+
+  document.getElementById("titDes").innerHTML = tabArmacoes[cod][0];
+  document.getElementById("imgDes").src = `Imagens/${tabArmacoes[cod][1]}.jpg`;
+  document.getElementById("prcDes").innerHTML =
+      `${tabArmacoes[cod][2]}<br/><br/>
+      Preço: R$ <span class="preco">${tabArmacoes[cod][3]},00</span>`;
+  
+}*/
+
+/*
+* QUESTÃO 4
+* Adicionar na lista e somar total
+* +
+* Mensagem de erro em caso de lista de pedidos vazia
+*/
+/*
+function incluirProd(){
+  lista = document.getElementById("selProd")
+  
+  if(lista.value == '-----'){
+      alert("Nenhum produto selecionado!")
+  }
+  else{
+    
+      document.getElementById("lisPedArea").innerHTML += lista.value + "\n"
+
+      op = lista.selectedIndex
+
+      if(op >= 1 && op <= 4){
+          parcial = tabLentes[op-1][3]
+      }
+      else{
+          parcial = tabArmacoes[op-5][3]
+      }
+
+      valor += parcial;
+      
+      document.getElementById("valorTotal").innerHTML = "Valor Total R$ " + valor + ",00\n"
+      
+      parcelarValor()
+  }
+}*/
+/*
+function parcelarValor(){
+  parc = document.getElementById("selParc")
+  if(parc.value == "à vista"){
+    parcela = 1
+  }
+  else{
+    parcela = parseFloat(parc.value)
+  }
+  document.getElementById("parcelas").innerHTML =
+            "Em " + parcela + "x de R$ " + Math.ceil(valor/parcela) + ",00\n"
+}
+
+function limpa(){
+  document.getElementById("lisPedArea").innerHTML = ""
+  document.getElementById("valorTotal").innerHTML = ""
+  document.getElementById("parcelas").innerHTML = ""
+}
+*/
